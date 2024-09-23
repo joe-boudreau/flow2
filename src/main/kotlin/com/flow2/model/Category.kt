79b34b1991b23fc36f62b22c94d45f3ee688dfc5
Joe Boudreau <joe.boudreau@protonmail.com>
2024-09-23 19:41:23 -0400

    tag and category links

--- src/main/kotlin/com/flow2/model/Category.kt
+++ src/main/kotlin/com/flow2/model/Category.kt
@@ -3,10 +3,13 @@ package com.flow2.model
 import kotlinx.serialization.Serializable
 
 @Serializable
-enum class Category {
-    BOOK_REVIEW,
-    TECH,
-    TRAVEL,
-    PERSONAL,
-    SIDI,
+enum class Category(
+    val displayName: String,
+    val urlName: String,
+) {
+    BOOK_REVIEW("Book Reviews", "book-reviews"),
+    TECH("Tech", "tech"),
+    TRAVEL("Travel", "travel"),
+    PERSONAL("Personal", "personal"),
+    SIDI("SIDI", "sidi"),
 }
