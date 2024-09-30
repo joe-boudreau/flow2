add768382f545c2a42ebf3cf1db6fc8cf1545d5e
Joe Boudreau <joe.boudreau@protonmail.com>
2024-09-30 16:22:10 -0400

    published date

--- src/main/kotlin/com/flow2/model/Post.kt
+++ src/main/kotlin/com/flow2/model/Post.kt
@@ -2,6 +2,7 @@ package com.flow2.model
 
 import org.bson.codecs.pojo.annotations.BsonId
 import org.bson.types.ObjectId
+import java.util.*
 
 data class Post(
     @BsonId val id: String = ObjectId().toString(),
@@ -13,7 +14,10 @@ data class Post(
     val publishedAt: Long,
     val updatedAt: Long,
     val slug: String = slugify(title)
-)
+) {
+    fun getPublishDate() = Date(publishedAt)
+    fun getUpdateDate() = Date(updatedAt)
+}
 
 private fun slugify(title: String) = title
     .replace(Regex("[^\\x00-\\x7F]"), "")
